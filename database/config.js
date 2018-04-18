dc32547a19abf49aca3971c1800042d49031d45f
Xing9527 <929885647@qq.com>
2018-04-18 19:14:06 +0800

    修改了一个样式

--- database/config.js
+++ database/config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/xing');
+mongoose.connect('mongodb://localhost/college');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
